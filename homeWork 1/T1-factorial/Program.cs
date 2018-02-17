1b6b9275fbfae0eb19218161bc0633d7b63dd0f2
Dmitriy Panfilyonok <31937213+anticNVM@users.noreply.github.com>
2018-02-17 15:11:26 +0300

    hw1.1/factorial (#1)
    * hw1.1\factorial complete
    
    * some necessary files added
    
    * deleted unnecessary files, codestyle fixed

---
+++ homeWork 1/T1-factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T1_factorial
+{
+    // Посчитать факториал
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = CorrectInput();
+            Console.WriteLine($"{input}! = {Factorial(input)}");
+        }
+
+        public static int Factorial(int stage)
+        {
+            if (stage == 0) 
+                return 1;
+
+            return stage * Factorial(stage - 1);
+        }
+
+        public static int CorrectInput()
+        {
+            Console.Write("Введите натуральное число для вычисления факториала: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            while (input < 0)
+            {
+                Console.WriteLine("Факториал определён только для натуральных числел.");
+                Console.Write("Пожалуйста, введите корректное значение: ");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            return input;
+        }
+    }
+}
