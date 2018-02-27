8e701e5fb70c4aa7b48e19bd47118b149424d22d
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-02-27 22:51:42 +0300

    init env

---
+++ homeWork 3/T1-StackCalculator/StackCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
