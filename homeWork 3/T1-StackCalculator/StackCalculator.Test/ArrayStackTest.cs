e2ae28818c9ab3d98b604a5e2994a96516e31a78
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-03-01 22:54:19 +0300

    Aome ArrayStack tests added

--- homeWork 3/T1-StackCalculator/StackCalculator.Test/ArrayStackTest.cs
+++ homeWork 3/T1-StackCalculator/StackCalculator.Test/ArrayStackTest.cs
@@ -7,16 +7,43 @@ namespace StackCalculator.Test
     {
         private static ArrayStack _stack;
 
-        // [TestInitialize]
-        // public void Init()
-        // {
-        //     _stack = new ArrayStack();
-        // }
+        [TestMethod]
+        public void StackResizingTest()
+        {
+            _stack = new ArrayStack(2);
+
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+
+            Assert.AreEqual(3, _stack.Count);
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void PopFromEmptyStackShouldReturnsNull()
         {
-            
+            _stack = new ArrayStack();
+
+            var result = _stack.Pop();
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void RightBehaviorOfStack()
+        {
+            _stack = new ArrayStack();
+
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+            var value1 = _stack.Pop();
+            var value2 = _stack.Pop();
+            var value3 = _stack.Pop();
+
+            Assert.AreEqual(3, value1);
+            Assert.AreEqual(2, value2);
+            Assert.AreEqual(1, value3);
         }
     }
 }
