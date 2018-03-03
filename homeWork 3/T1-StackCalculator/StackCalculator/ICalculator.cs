99980dcf5af4db6f62f78588ddec66e379331d83
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-03-03 14:56:18 +0300

    Docs added

--- homeWork 3/T1-StackCalculator/StackCalculator/ICalculator.cs
+++ homeWork 3/T1-StackCalculator/StackCalculator/ICalculator.cs
@@ -1,7 +1,15 @@
 namespace StackCalculator
 {
+    /// <summary>
+    /// Реализует функциональность калькулятора для данного выражения.
+    /// </summary>
     public interface ICalculator
     {
+        /// <summary>
+        /// Вычисляет результат арифметического выражения
+        /// </summary>
+        /// <param name="expression">Вычисляемое арифметическое выражение.</param>
+        /// <returns>Целочистенный результат выражения или .null, если выражение некорректно.</returns>
         int? Calculate(string expression);
     }
 }
--- homeWork 3/T1-StackCalculator/StackCalculator/IStack.cs
+++ homeWork 3/T1-StackCalculator/StackCalculator/IStack.cs
@@ -1,12 +1,43 @@
 namespace StackCalculator
 {
+    /// <summary>
+    /// Интерфейс, реализующий функциональность стека.
+    /// </summary>
     public interface IStack
     {
+        /// <summary>
+        /// Добавляет значение в стек.
+        /// </summary>
+        /// <param name="value">Добавляемое значение.</param>
         void Push(int value);
+
+        /// <summary>
+        /// Забирает значение из вершины стека.
+        /// </summary>
+        /// <returns>Элемент на вершине стека (.null если стек пуст).</returns>
         int? Pop();
+
+        /// <summary>
+        /// Возвращает значение из вершины стека (не удаляя его).
+        /// </summary>
+        /// <returns>Элемент на вершине стека (.null если стек пуст).</returns>
         int? Peek();
+
+        /// <summary>
+        /// Очищает стек, удаляя из него все элементы.
+        /// </summary>
         void Clear();
+
+        /// <summary>
+        /// Проверяет стек на пустоту.
+        /// </summary>
+        /// <returns>.true если стек пуст, иначе .false</returns>
         bool IsEmpty();
+
+        /// <summary>
+        /// Свойство, определяющее размер стека (кол-во элементов в нем).
+        /// </summary>
+        /// <returns>Количество элементов в стеке.</returns>
         int Count { get; }
     }
 }
