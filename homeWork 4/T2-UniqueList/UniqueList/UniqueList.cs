86558faaedc71974584768c5dd5508aedef8779a
Dmitriy Panfilyonok <31937213+anticNVM@users.noreply.github.com>
2018-03-09 23:51:59 +0300

    Hw4.2 UniqueList (#8)
    * Создание рабочего окружения
    
    * Добавлены классы исключений, UniqueList, protected head, tail в LinkedList
    
    * Изменена структура каталогов: файли и папки объединены в папку T2-UniqueList
    
    * Удаление лишних файлов каталогов
    
    * Добавлен индексер для списка, тесты, пофикшены баги (не все)
    
    * Теперь, благодаря виртуальным методам, UniqueList переопределяет методы-предки верно, переименованны тесты
    
    * Добавленна документация к тестам, измененна область видимости полей в LinkedList

---
+++ homeWork 4/T2-UniqueList/UniqueList/UniqueList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace UList
+{
+    public class UniqueList : LinkedList.LinkedList
+    {
+        public override void Append(int value)
+        {
+            if (this.Contains(value))
+            {
+                throw new Exceptions.ValueAlreadyInListException(
+                    String.Format($"Значение параметра {nameof(value)} уже существует в списке")
+                );
+            }
+
+            base.Append(value);
+        }
+
+        public override void AddToBegin(int value)
+        {
+            if (this.Contains(value))
+            {
+                throw new Exceptions.ValueAlreadyInListException(
+                    String.Format($"Значение параметра {nameof(value)} уже существует в списке")
+                );
+            }
+
+            base.AddToBegin(value);
+        }
+
+        public override void Insert(int value, int after)
+        {
+            if (this.Contains(value))
+            {
+                throw new Exceptions.ValueAlreadyInListException(
+                    String.Format($"Значение параметра {nameof(value)} уже существует в списке")
+                );
+            }
+
+            base.Insert(value, after);
+        }
+    }
+}
