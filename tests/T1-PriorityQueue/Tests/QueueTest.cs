1372f25c8b4360f5988e9b0348345a197f138194
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-04-03 12:59:06 +0300

    Добавленны комментарии к тестам

--- tests/T1-PriorityQueue/Tests/QueueTest.cs
+++ tests/T1-PriorityQueue/Tests/QueueTest.cs
@@ -4,6 +4,9 @@ using System;
 
 namespace Tests
 {
+    /// <summary>
+    /// Класс, тестирующий методы очереди с приоритетом
+    /// </summary>
     [TestClass]
     public class QueueTest
     {
@@ -15,6 +18,9 @@ namespace Tests
             _queue = new Queue<int>();
         }
 
+        /// <summary>
+        /// Попытка получить элемент из пустой очереди
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(QueueSource.Exceptions.QueueIsEmptyException))]
         public void DequeFromEmptyQueShouldThrowException()
@@ -22,6 +28,9 @@ namespace Tests
             _queue.Dequeue();
         }
 
+        /// <summary>
+        /// Корректное поведение очереди при 1 элементе (smoke test)
+        /// </summary>
         [TestMethod]
         public void EnqueOneElementBehaviorTest()
         {
@@ -34,6 +43,9 @@ namespace Tests
             Assert.IsTrue(_queue.IsEmpty());
         }
 
+        /// <summary>
+        /// Проверка корректной работы для разных приоритетов (должно возвращать наименьший)
+        /// </summary>
         [TestMethod]
         public void EnqueTwoElementsWithDifferentPriorities()
         {
@@ -46,12 +58,31 @@ namespace Tests
             Assert.AreEqual(expected, value);
         }
 
+        /// <summary>
+        /// Проверка корректности при одинаковых приоритетах (должно вести себя как очередь)
+        /// </summary>
         [TestMethod]
         public void EnqueTwoElementsWithEqualPriorities()
         {
             var expected = 10;
             _queue.Enqueue(expected, 1);
             _queue.Enqueue(5, 1);
+
+            var value = _queue.Dequeue();
+
+            Assert.AreEqual(expected, value);
+        }
+
+        /// <summary>
+        /// Ещё 1 тест, который совмещает 2 предыдущие
+        /// </summary>
+        [TestMethod]
+        public void EnqueThreeElementsWithBehaviorTest()
+        {
+            var expected = 10;
+            _queue.Enqueue(expected, -1);
+            _queue.Enqueue(5, 1);
+            _queue.Enqueue(0, -1);
 
             var value = _queue.Dequeue();
 
