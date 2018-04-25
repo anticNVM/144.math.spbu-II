6895bbe6a2a84aedd214e5911df99c554d70c316
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-04-25 19:54:32 +0300

    init

---
+++ homeWork 9/T1-PrimeGenerator/PrimeGeneratorSource/PrimeGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeGeneratorSource
+{
+    public static class PrimeGenerator
+    {
+        public static IEnumerable<int> Generate()
+        {
+            yield return 1;
+        }
+    }
+}
