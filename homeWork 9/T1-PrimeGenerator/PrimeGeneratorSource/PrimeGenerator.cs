80c8223aacf6c5596788c70285c77ba7e9663678
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-05-17 21:32:28 +0300

    Добавлена логика программы

--- homeWork 9/T1-PrimeGenerator/PrimeGeneratorSource/PrimeGenerator.cs
+++ homeWork 9/T1-PrimeGenerator/PrimeGeneratorSource/PrimeGenerator.cs
@@ -7,7 +7,48 @@ namespace PrimeGeneratorSource
     {
         public static IEnumerable<int> Generate()
         {
-            yield return 1;
+            return Filter<int>(GenerateInts(), IsPrime);
+        }
+
+        public static IEnumerable<int> GenerateInts()
+        {
+            int current = 0;
+            while (true)
+            {
+                yield return current++;
+            }
+        }
+
+        private static IEnumerable<T> Filter<T>(IEnumerable<T> collection, Predicate<T> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number == 1)
+            {
+                return false;
+            }
+
+            var i = 2;
+            while (i * i <= number)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+
+                i++;
+            }
+
+            return true;
         }
     }
 }
