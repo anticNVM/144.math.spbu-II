15dab67c0a011a9b592a277741610820b4d530c3
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-05-26 15:11:57 +0300

    Game from unity-tutorial created

---
+++ homeWork 9/UFO Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject _player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = this.transform.position - _player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		this.transform.position = _player.transform.position + offset;
+	}
+}
---
+++ homeWork 9/UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _playersRB;
+    private int _count;
+	private int _amountOfPickups;
+
+    public float _speed;
+    public Text _scoreText;
+	public Text _winText;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    public void Start()
+    {
+        _playersRB = GetComponent<Rigidbody2D>();
+		_amountOfPickups = GameObject.FindGameObjectsWithTag("PickUp").Length;
+    }
+
+    /// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	public void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector2(moveHorizontal, moveVertical);
+
+        _playersRB.AddForce(movement * _speed);
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            UpdateScoreText();
+        }
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreText.text = "Score: " + _count.ToString();
+		if (_count >= _amountOfPickups)
+		{
+			_winText.text = "You Win!!!";
+		}
+    }
+}
