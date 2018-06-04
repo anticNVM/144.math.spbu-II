658170a9f34271a7f7207aedcde7cd4efbbf3a2a
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-06-05 02:23:08 +0300

    Настройки часов и запуск таймера вынесены в отдельные методы

--- homeWork 6/T2-DigitalClock/DigitalClockSource/MainWindow.cs
+++ homeWork 6/T2-DigitalClock/DigitalClockSource/MainWindow.cs
@@ -4,30 +4,40 @@ using System.Timers;
 
 public partial class MainWindow : Gtk.Window
 {
-    private Timer timer;
+    private Timer _timer;
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
 
+        SetClockLabelSettings();
+        InitializeAndStartTimer();
+    }
+
+    private void SetClockLabelSettings()
+    {
         label6.Text = System.DateTime.Now.ToLongTimeString();
-        label6.ModifyBg(StateType.Normal, new Gdk.Color(255, 0, 255));
+        //label6.ModifyBg(StateType.Normal, new Gdk.Color(255, 0, 255));
         label6.ModifyFg(StateType.Normal, new Gdk.Color(0, 255, 0));
         label6.ModifyFont(Pango.FontDescription.FromString("Monospace 36"));
+    }
 
-        timer = new Timer(1000);
-        timer.Elapsed += OnTimedEvent;
-        timer.Start();
+    private void InitializeAndStartTimer()
+    {
+        _timer = new Timer(1000);
+        _timer.Elapsed += OnTimedEvent;
+        _timer.Start();
     }
 
     protected void OnTimedEvent(object source, ElapsedEventArgs e)
     {
-        //label6.Text = System.DateTime.Now.ToLongTimeString();
+        label6.Text = System.DateTime.Now.ToLongTimeString();
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
-        timer.Close();
+        _timer.Close();
+
         Application.Quit();
         a.RetVal = true;
     }
