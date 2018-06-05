48e1d0577d009facd9104f0112cbf6aa28a9c797
Dmitriy Panfilyonok <panfilen-ok@mail.ru>
2018-06-05 21:44:00 +0300

    Начал писать тесты - получается не очень(

---
+++ homeWork 5/T2-MapWalker/Tests/MapBehaviorTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Source;
+
+namespace Tests
+{
+    [TestClass]
+    public class MapBehaviorTest
+    {
+        private IMap _map;
+
+        [TestInitialize]
+        public void Init()
+        {
+            // _map = new Map();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
